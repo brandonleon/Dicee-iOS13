1273d272f213d6a35219e78741b629d38827312d
Brandon Leon <brandon.leon@servicenow.com>
2022-05-23 13:44:47 -0700

    Refactoring and generate random numbers for dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,18 +11,14 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
-    
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-
-        
-    }
+    var diceImageArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
     @IBAction func rollButtonPress(_ sender: Any) {
-        [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceFour")]
+        diceImageView1.image = diceImageArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceImageArray[Int.random(in: 0...5)]
+
     }
     
 }
