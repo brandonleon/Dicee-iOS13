5ac7f76d413831e42e672190de15c215f1f76a4f
Brandon Leon <brandonleon@pm.me>
2022-05-23 12:36:00 -0700

    Added Roll button and dice views.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+
+        
     }
 
-
+    @IBAction func rollButtonPress(_ sender: Any) {
+        [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceFour")]
+    }
+    
 }
 
